be132fa8b0621ffb992445da265322e09faeb114
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-09-28 14:29:59 -0400

    chore(core): add room to watch

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-console.log(process.env);
+//console.log(process.env);
+
+process.env.HUBOT_ADAPTER = 'gitter2';
+process.env.HUBOT_GITTER2_TOKEN = '***REMOVED***';
 
 require('coffee-script/register');
 module.exports = require('hubot/bin/hubot.coffee');
