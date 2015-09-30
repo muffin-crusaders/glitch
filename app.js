fb3ffd9a0448b9a7bdab77a9831e5d3bf7a925ac
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-09-30 14:15:10 -0400

    feat(core): remove notification from post

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 //console.log(process.env);
 
-process.env.HUBOT_ADAPTER = 'gitter2';
-process.env.HUBOT_GITTER2_TOKEN = 'b2699df219531e128317c2ac8e736f38097c6096';
-
 require('coffee-script/register');
 module.exports = require('hubot/bin/hubot.coffee');
