62dca6f47dc443e2b01ca77888c7c1af94a0e53b
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-11-09 11:15:23 -0500

    feat(PR): check if PR is assigned to someone
    leave a comment if it isn't

---
+++ scripts/check-pr-assignee.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys, getopt
+from github import Github
+
+def main(argv):
+    opts, args = getopt.getopt(argv,"r:p:t:")
+
+    for opt, arg in opts:
+        if opt == "-p":
+            pull = int(arg)
+        elif opt == "-r":
+            repo = arg.strip()
+        elif opt == "-t":
+            token = arg.strip()
+
+    print pull, repo
+
+    # First create a Github instance:
+    g = Github(token)
+
+    # post a comment to an issue
+    if g.get_repo(repo).get_issue(pull).assignee == None:
+
+        g.get_repo(repo).get_issue(pull).create_comment("You forgot to assign your PR! It's a muffin offence, you know...")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
